79d97c4107d310ca9dee72d3af1188a9b2dffa37
tahir76270 <motahir76270>
2025-09-07 09:57:57 +0530

    resume link change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1WS0ALk0e1nrGSLvMpizsNXSIPTuiaWxG/view?usp=sharing"
+            href="https://drive.google.com/file/d/1dc1RX8xrUWxYH4jBpl6NcXQOLIU4GSzn/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
